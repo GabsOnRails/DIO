f6b30e310f6159c8cc48fe06543d9aafa445f9ee
GabsOnRails <gabrielfelipe.oliveira2000@gmail.com>
2025-04-28 16:10:27 -0300

    DIO AKS e Kubernets

--- dio_cloud_native/ambiente_virtual/Scripts/activate.bat
+++ dio_cloud_native/ambiente_virtual/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\conta\OneDrive\Documents\GitHub\DIO\dio_cloud_native\ambiente_virtual
+set VIRTUAL_ENV=C:\Users\GabrielFelipedeOlive\Desktop\DIO\dio_cloud_native\ambiente_virtual
 
 if not defined PROMPT set PROMPT=$P$G
 
